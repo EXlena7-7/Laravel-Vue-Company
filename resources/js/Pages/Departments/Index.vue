f426cc366d878fed69509909a9f2f01573d3fa27
Marjory Elena <109706655+EXlena7-7@users.noreply.github.com>
2025-05-08 15:27:41 -0400

    Fullstack Laravel y Vue3

---
+++ resources/js/Pages/Departments/Index.vue
@@ -0,0 +1,40 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import DangerButton from '@/Components/DangerButton.vue';
+import {Head, Link, useForm} from '@inertiajs/vue3';
+import { Head, useForm } from '@inertiajs/vue3';
+import Swal from 'sweetalert2';
+
+const props = defineProps({
+    departments: {type:Object}
+});
+
+const form = useForm({
+    id:''
+});
+
+</script>
+
+<template>
+    <Head title=" Departments" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800"
+            >
+                Departments
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div
+                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
+                >
+
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
