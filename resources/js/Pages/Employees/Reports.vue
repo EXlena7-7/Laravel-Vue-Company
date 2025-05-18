8aa3d492b6f6db85bae1493871f841aaf0a11fcd
Marjory Elena <109706655+EXlena7-7@users.noreply.github.com>
2025-05-18 18:28:12 -0400

    🧑‍💻 Fullstack Laravel y Vue3 Company
    Fullstack Laravel y Vue3 Company

--- resources/js/Pages/Employees/Reports.vue
+++ resources/js/Pages/Employees/Reports.vue
@@ -5,9 +5,10 @@ import { ref } from 'vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import SelectInput from '@/Components/SelectInput.vue';
 import DataTable from 'datatables.net-vue3';
+import 'datatables.net-dt/css/dataTables.dataTables.css';
 import ButtonsHtml5 from 'datatables.net-buttons/js/buttons.html5';
 import ButtonsPrint from 'datatables.net-buttons/js/buttons.print';
-import Responsive from 'datatables.net-responsive';
+import Responsive from 'datatables.net-responsive-dt';
 import 'datatables.net-responsive-dt/css/responsive.dataTables.min.css';
 import JsZip from 'jszip';
 import pdfmake from 'pdfmake/build/pdfmake';
@@ -62,7 +63,7 @@ buttons1.value =[
      },
      {
         title:'Employees', extend:'copy', text:'<i class="fa-solid fa-copy"></i> COPY',
-        className:'inline-flex items-center rounded-md border border-transparent bg-gray-200 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900'
+        className:'inline-flex items-center rounded-md border border-gray-800 bg-gray-200 px-4 py-2 text-xs font-semibold uppercase tracking-widest transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900'
      }
 ],
 
@@ -81,7 +82,7 @@ buttons2.value =[
      },
      {
         title:'Departments', extend:'copy', text:'<i class="fa-solid fa-copy"></i> COPY',
-        className:'inline-flex items-center rounded-md border border-transparent bg-gray-200 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900'
+        className:'inline-flex items-center rounded-md border border-gray-800 bg-gray-200 px-4 py-2 text-xs font-semibold uppercase tracking-widest transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900'
      }
 ]
 
@@ -110,7 +111,7 @@ buttons2.value =[
                 </div>
                 <div v-if="report == '1'" class="px-6 py-6 overflow-hidden bg-white shadow-sm sm:rounded-lg">
                     <DataTable :data="employees" :columns="columns1"
-                    class="w-full border-gray-400"
+                    class="w-full border border-gray-400"
                     :options="{responsive:true, autoWidth:false,dom:'Bfrtip',buttons:buttons1}">
                     <thead>
                         <tr class="bg-gray-100">
@@ -125,7 +126,16 @@ buttons2.value =[
                     </DataTable>
                 </div>
                 <div v-else class="px-6 py-6 overflow-hidden bg-white shadow-sm sm:rounded-lg">
-
+                    <DataTable :data="departments" :columns="columns2"
+                    class="w-full border border-gray-400"
+                    :options="{responsive:true, autoWidth:false,dom:'Bfrtip',buttons:buttons2}">
+                    <thead>
+                        <tr class="bg-gray-100">
+                            <th class="px-2 py-2">#</th>
+                            <th class="px-2 py-2">NAME</th>
+                        </tr>
+                    </thead>
+                    </DataTable>
                 </div>
             </div>
         </div>
