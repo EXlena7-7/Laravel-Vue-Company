47d65aa1debcc1be8dca9897d9d206e7d269b2f2
Marjory Elena <109706655+EXlena7-7@users.noreply.github.com>
2025-05-29 18:30:17 -0400

    Personalizacion

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -5,7 +5,12 @@ import { Link } from '@inertiajs/vue3';
 
 <template>
     <div
-        class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0"
+        class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 bg-overlay"
+         :style="{
+        backgroundImage: 'url(fondo.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    }"
     >
         <div>
             <Link href="/">
@@ -20,3 +25,17 @@ import { Link } from '@inertiajs/vue3';
         </div>
     </div>
 </template>
+<style>
+.bg-overlay::before {
+  content: "";
+  position: absolute;
+  inset: 0;
+  background: rgba(255,255,255,0.6); /* Ajusta la opacidad aquí */
+  z-index: 1;
+  pointer-events: none;
+}
+.bg-overlay > * {
+  position: relative;
+  z-index: 2;
+}
+</style>
